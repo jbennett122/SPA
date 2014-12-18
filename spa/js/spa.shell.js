841053811790f1a733aade3d5499fd7a4445382b
jbennett122 <justinbennett122@yahoo.com>
2014-12-18 11:44:40 -0500

    ??

--- spa/js/spa.shell.js
+++ spa/js/spa.shell.js
@@ -1,6 +1,6 @@
 /*
 * spa.shell.js
-* Shell module for SPA
+* Shell modulje for SPA
 */
 /*jslint browser : true, continue : true,
 devel : true, indent : 2, maxerr : 50,
@@ -9,39 +9,98 @@ regexp : true, sloppy : true, vars : false,
 white : true
 */
 /*global $, spa */
-spa.shell = (function () {
+spa.shell = (function (){
 //---------------- BEGIN MODULE SCOPE VARIABLES --------------
-var
-configMap = {
-main_html : String()
-  + '<div class="spa-shell-head">'
-    + '<div class="spa-shell-head-logo"></div>'
-    + '<div class="spa-shell-head-acct"></div>'
-    + ' <div class="spa-shell-head-search"></div>'
-  + '</div>'
-
-    + '<div class="spa-shell-main">'
-      + '<div class="spa-shell-main-nav"><h1>Hi there</h1></div>'
-      + '<div class="spa-shell-main-content"></div>'
+ var configMap = {
+  main_html : String()
+    + '<div class="spa-shell-head">'
+      + '<div class="spa-shell-head-logo">logo</div>'
+      + '<div class="spa-shell-head-acct">account</div>'
+      + ' <div class="spa-shell-head-search">search</div>'
     + '</div>'
 
-    + '<div class="spa-shell-foot"></div>'
-    + '<div class="spa-shell-chat"></div>'
-    + '<div class="spa-shell-modal"></div>'
+      + '<div class="spa-shell-main">'
+        + '<div class="spa-shell-main-nav"><h1>Hi there</h1></div>'
+        + '<div class="spa-shell-main-content">main content</div>'
+      + '</div>'
+
+      + '<div class="spa-shell-foot">footer</div>'
+      + '<div class="spa-shell-chat">chat</div>'
+      + '<div class="spa-shell-modal"></div>',
+	
+	chat_extend_time: 1000,
+	chat_retract_time: 300,
+	chat_extend_height:450,
+	chat_retract_height:15	
+	 
 },
 stateMap = { $container : null },
 jqueryMap = {},
-setJqueryMap, initModule;
+
+setJqueryMap, toggleChat, initModule;
 //----------------- END MODULE SCOPE VARIABLES ---------------
 //-------------------- BEGIN UTILITY METHODS -----------------
 //--------------------- END UTILITY METHODS ------------------
 //--------------------- BEGIN DOM METHODS --------------------
-// Begin DOM method /setJqueryMap/
+// Begin DOM method----/setJqueryMap/-------------------------
 setJqueryMap = function () {
 var $container = stateMap.$container;
-jqueryMap = { $container : $container };
+
+  jqueryMap = {
+   $container : $container,
+   $chat : $container.find('.spa-shell-chat')
+  };
 };
-// End DOM method /setJqueryMap/
+//--------------------/setJqueryMap/--------------------------
+//Begin---------------/toggleChat/----------------------------
+//Purpose: Extends or retracts slider
+//Arguments:
+// * do_extend if true, extends slider, if false retracts
+// * callback -optional function to execute at end of animation
+//Settings:
+// * chat_extend_time, chat_retract_time
+// * chat_extend_height, chat_retract_height
+//Returns: boolean
+// * true - slider animation activated
+// * false - slider not activated
+//
+toggleChat = function(do_extend,callback){
+ var
+  px_chat_ht = jqueryMap.$chat.height(),
+  is_open = px_chat_ht === configMap.chat_extend_height,
+  is_closed = px_chat_ht === configMap.chat_retract_height,
+  is_sliding = !is_open && !is_closed;
+  
+ //avoid race condition
+ if (is_sliding){return false;}
+ 
+ //begin chat slider if false slider is not extended
+ if (do_extend) {
+  jqueryMap.$chat.animate(
+    {height: configMap.chat_extend_height},
+	 configMap.chat_extend_time,
+	  function(){
+	   if (callback) {callback(jqueryMap.$chat);}
+	  }
+  );
+    return true;
+	
+	}
+// End extend chat slider
+// Begin retract chat slider
+    jqueryMap.$chat.animate(
+      { height : configMap.chat_retract_height },
+        configMap.chat_retract_time,
+        function () {
+          if ( callback ){ callback( jqueryMap.$chat ); }
+        }
+      );
+   return true;
+};
+//--------------end chat slider-----------------------------------
+
+
+
 //--------------------- END DOM METHODS ----------------------
 //------------------- BEGIN EVENT HANDLERS -------------------
 //-------------------- END EVENT HANDLERS --------------------
@@ -51,8 +110,14 @@ initModule = function ( $container ) {
 stateMap.$container = $container;
 $container.html( configMap.main_html );
 setJqueryMap();
+
+// test toggle
+setTimeout( function () {toggleChat( true ); }, 3000 );
+setTimeout( function () {toggleChat( false );}, 8000 );
 };
+
 // End PUBLIC method /initModule/
 return { initModule : initModule };
+
 //------------------- END PUBLIC METHODS ---------------------
 }());
